7b87eec614fa98573aa9aba8dac10bd2c4a68106
Hannah Redmond <hannah.redmond@baesystemsdetica.com>
2018-06-29 17:23:21 +0100

    minor changes for formatting

--- src/lib/components/wordCloudElement/wordCloudElement.jsx
+++ src/lib/components/wordCloudElement/wordCloudElement.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
 
-import './wordCloudElement.css';
+import "./wordCloudElement.css";
 
 export default class WordCloudElement extends Component {
   static propTypes = {
@@ -15,20 +15,20 @@ export default class WordCloudElement extends Component {
     growOnHover: PropTypes.bool,
     handleLeftClick: PropTypes.func,
     handleRightClick: PropTypes.func
-  }
+  };
 
   static defaultProps = {
-    col: 'rgb(0,0,255)',
+    col: "rgb(0,0,255)",
     opacity: 1.0,
-    font: 'roboto',
+    font: "roboto",
     growOnHover: true
-  }
+  };
 
   constructor(props) {
     super(props);
     this.state = {
-                   hover : false
-                 }
+      hover: false
+    };
   }
 
   render() {
@@ -39,30 +39,35 @@ export default class WordCloudElement extends Component {
     var fontSize = this.props.fontSize;
     var font = this.props.font;
     const col = this.props.col;
-    const opacity = this.props.opacity
+    const opacity = this.props.opacity;
     if (this.props.growOnHover && this.state.hover) {
       fontSize *= 1.15;
     }
-    fontSize=fontSize.toString()+'px';
+    fontSize = fontSize.toString() + "px";
     const style = {
-                    fontSize   : fontSize,
-                    fontFamily : font,
-                    fill       : col,
-                    cursor     : 'pointer',
-                    opacity    : opacity
-                  };
+      fontSize: fontSize,
+      fontFamily: font,
+      fill: col,
+      cursor: "pointer",
+      opacity: opacity
+    };
     return (
-
-<text id={'word-cloud-element-' + this.props.originalIndex} tabIndex={index} style={style} focusable="true" textAnchor='middle' alignmentBaseline='central' x={x} y={y}
-      onFocus={()=>this.props.updateCurrentFocusDescription(index, text)}
-      onMouseOver={()=>this.setState( { hover : true })}
-      onMouseOut={()=>this.setState( { hover : false })}
-      onClick={this.props.handleLeftClick} 
-      onContextMenu={this.props.handleRightClick}
->
-  {text}
-</text>
+      <text
+        id={"word-cloud-element-" + this.props.originalIndex}
+        tabIndex={index}
+        style={style}
+        focusable="true"
+        textAnchor="middle"
+        alignmentBaseline="central"
+        x={x}
+        y={y}
+        onFocus={() => this.props.updateCurrentFocusDescription(index, text)}
+        onMouseOver={() => this.setState({ hover: true })}
+        onMouseOut={() => this.setState({ hover: false })}
+        onClick={this.props.handleLeftClick}
+        onContextMenu={this.props.handleRightClick}>
+        {text}
+      </text>
     );
   }
 }
-
--- src/lib/index.js
+++ src/lib/index.js
@@ -9,26 +9,24 @@
   4) Data consists of a list of pairs (text, score). Would like multiple versions of
    text so we can display translations.
 */
-
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import WordCloudElement from './components/wordCloudElement/wordCloudElement';
 import { computeLayout } from './services/layout.service';
-import {isEqual} from 'lodash';
+import { isEqual } from 'lodash';
 
 class WordCloud extends Component {
-
     static propTypes = {
         data: PropTypes
             .arrayOf(PropTypes.shape({
-            word: PropTypes.string.isRequired,
-            score: PropTypes
-                .oneOfType([
-                PropTypes.number, PropTypes.objectOf(PropTypes.number)
-            ])
-                .isRequired,
-            translation: PropTypes.objectOf(PropTypes.string)
-        }))
+                word: PropTypes.string.isRequired,
+                score: PropTypes
+                    .oneOfType([
+                        PropTypes.number, PropTypes.objectOf(PropTypes.number)
+                    ])
+                    .isRequired,
+                translation: PropTypes.objectOf(PropTypes.string)
+            }))
             .isRequired,
         scoreKey: PropTypes.string, //Can be set to one of the keys in score
         textKey: PropTypes.string, //Can be set to one of the keys in translations
@@ -42,7 +40,6 @@ class WordCloud extends Component {
         handleLeftClick: PropTypes.func,
         handleRightClick: PropTypes.func
     }
-
     static defaultProps = {
         scoreKey: null,
         textKey: null,
@@ -54,7 +51,6 @@ class WordCloud extends Component {
         highlightWords: null,
         highlightWordsIndexes: null
     }
-
     constructor(props) {
         super(props);
         this.state = {
@@ -63,10 +59,8 @@ class WordCloud extends Component {
             scores: [],
             accessibleContentDescription: ''
         };
-
         this.updateFocusDescription = this.updateFocusDescription.bind(this);
     }
-
     shouldHighlightWord = (index) => {
         const word = this.props.data[index].word
         if (this.props.highlightWords === null && this.props.highlightWordsIndexes === null) {
@@ -78,22 +72,19 @@ class WordCloud extends Component {
             return (this.props.highlightWordsIndexes.indexOf(index) >= 0)
         }
     }
-
     /*Function to measure the bounding boxes of all the child svg's*/
     measure = () => {
         const childElements = Array.from(this.svg.children);
         const dimensions = childElements
             .map(element => element.getBoundingClientRect())
-            .map(({width, height}) => ({width, height}));
+            .map(({ width, height }) => ({ width, height }));
         return dimensions;
     }
-
     storeSvgReference = svg => {
         if (svg !== null) {
             this.svg = svg;
         }
     };
-
     getTextAndScoreKey = (props) => {
         let textKey
         let scoreKey
@@ -107,12 +98,11 @@ class WordCloud extends Component {
         } else {
             scoreKey = props.scoreKey
         }
-        return {textKey, scoreKey}
+        return { textKey, scoreKey }
     }
-
     getWordsAndScores = (props) => {
         /*Which list of words with scores should we render*/
-        let {textKey, scoreKey} = this.getTextAndScoreKey(props)
+        let { textKey, scoreKey } = this.getTextAndScoreKey(props)
         let text
         let scores
         if (props.textKey == null) {
@@ -134,19 +124,23 @@ class WordCloud extends Component {
                 .data
                 .map(x => x.score[props.scoreKey])
         }
-        return {textKey, scoreKey, text, scores}
+        return { textKey, scoreKey, text, scores }
     }
-
     computeLayoutFromDimensions = (props, dimensions) => {
-        let {textKey, scoreKey, text, scores} = this.getWordsAndScores(props)
+        let { textKey, scoreKey, text, scores } = this.getWordsAndScores(props)
         var layout = {
             ...this.state.layout
         }
         if (!layout.hasOwnProperty(textKey)) {
             layout[textKey] = {}
         }
-        layout[textKey][scoreKey] = text.map((w, i) => ({text: w, index : i, value: scores[i], 
-            width: dimensions[i]['width'], height: dimensions[i]['height'], visible: props.data[i].visible 
+        layout[textKey][scoreKey] = text.map((w, i) => ({
+            text: w,
+            index: i,
+            value: scores[i],
+            width: dimensions[i]['width'],
+            height: dimensions[i]['height'],
+            visible: props.data[i].visible
         }));
         layout[textKey][scoreKey] = layout[textKey][scoreKey].filter(x => props.data[x.index].visible)
         layout[textKey][scoreKey] = computeLayout(layout[textKey][scoreKey], props.width, props.height, props.padding);
@@ -157,49 +151,44 @@ class WordCloud extends Component {
             isMeasured[textKey] = {}
         }
         isMeasured[textKey][scoreKey] = true
-        this.setState({layout, isMeasured});
+        this.setState({ layout, isMeasured });
     }
-
     recomputeLayout = (props) => {
-        let {textKey, scoreKey} = this.getTextAndScoreKey(props)
+        let { textKey, scoreKey } = this.getTextAndScoreKey(props)
         var layout = {
             ...this.state.layout
         };
         layout[textKey][scoreKey] = layout[textKey][scoreKey].filter(x => props.data[x.index].visible)
         layout[textKey][scoreKey] = computeLayout(layout[textKey][scoreKey], props.width, props.height, props.padding);
-        this.setState({layout});
+        this.setState({ layout });
     }
-
     componentDidMount() {
         /*We must measure the rendered components*/
         const dimensions = this.measure();
         this.computeLayoutFromDimensions(this.props, dimensions);
     }
-
     componentDidUpdate() {
-        let {textKey, scoreKey} = this.getTextAndScoreKey(this.props)
+        let { textKey, scoreKey } = this.getTextAndScoreKey(this.props)
         let isMeasured = this.state.isMeasured;
         if (!isMeasured.hasOwnProperty(textKey) || !isMeasured[textKey].hasOwnProperty(scoreKey) || isMeasured[textKey][scoreKey] === false) {
             const dimensions = this.measure()
             this.computeLayoutFromDimensions(this.props, dimensions)
         }
     }
-
     componentWillReceiveProps(nextProps) {
         if (!isEqual(this.props.data, nextProps.data)) {
-            this.setState(prevState => ({layout: {}, isMeasured: {}}));
+            this.setState(prevState => ({ layout: {}, isMeasured: {} }));
             return
         }
         if (nextProps.width !== this.props.width || nextProps.height !== this.props.height || nextProps.padding !== this.props.padding) {
-            let {textKey, scoreKey} = this.getTextAndScoreKey(nextProps)
+            let { textKey, scoreKey } = this.getTextAndScoreKey(nextProps)
             let isMeasured = this.state.isMeasured;
             if (isMeasured.hasOwnProperty(textKey) && isMeasured[textKey].hasOwnProperty(scoreKey) && isMeasured[textKey][scoreKey] === true) {
                 this.recomputeLayout(nextProps);
             }
-        }
-        else {
+        } else {
             /*Has any of the visibility changed*/
-            let {textKey, scoreKey} = this.getTextAndScoreKey(nextProps)
+            let { textKey, scoreKey } = this.getTextAndScoreKey(nextProps)
             let isMeasured = this.state.isMeasured;
             if (isMeasured.hasOwnProperty(textKey) && isMeasured[textKey].hasOwnProperty(scoreKey) && isMeasured[textKey][scoreKey] === true) {
                 let layout = this.state.layout[textKey][scoreKey]
@@ -216,21 +205,16 @@ class WordCloud extends Component {
             }
         }
     }
-
     updateFocusDescription(index, text) {
         let current = this.state.layout.__text__.freq.find(item => item.text == text);
-
-        let output = current.text + " with score " +  current.value;
+        let output = current.text + " with score " + current.value;
         if (this.state !== undefined) {
-            this.setState(prevState => ({accessibleContentDescription : output}))
+            this.setState(prevState => ({ accessibleContentDescription: output }))
         }
-
     }
-
     render() {
-        let {textKey, scoreKey, text, scores} = this.getWordsAndScores(this.props)
+        let { textKey, scoreKey, text, scores } = this.getWordsAndScores(this.props)
         let isMeasured = this.state.isMeasured;
-        
         if (!isMeasured.hasOwnProperty(textKey) || !isMeasured[textKey].hasOwnProperty(scoreKey) || isMeasured[textKey][scoreKey] === false) {
             /*Render all the text invisibly to the screen and mesure the size of the svg elements*/
             const elementsToMeasure = text.map((w, i) => <WordCloudElement
@@ -252,15 +236,13 @@ class WordCloud extends Component {
         const width = this.props.width;
         const height = this.props.height;
         const layout = this.state.layout[textKey][scoreKey];
-
         const wordCloudElements = layout
             .filter(d => d.fontSize > this.props.minFontSize)
             .map((d, i) => {
                 let opacity
-                if (this.props.highlightWords.length === 0 || this.props.highlightWords[0].length === 0|| this.shouldHighlightWord(d.index)) {
+                if (this.props.highlightWords.length === 0 || this.props.highlightWords[0].length === 0 || this.shouldHighlightWord(d.index)) {
                     opacity = 1.0
-                }
-                else {
+                } else {
                     opacity = 0.25
                 }
                 return <WordCloudElement
@@ -285,10 +267,7 @@ class WordCloud extends Component {
                 {wordCloudElements}
             </svg>
             <div className='sr-only_' hidden={this.state.accessibleContentDescription.length === 0} aria-live="polite">Focused word: {this.state.accessibleContentDescription}</div>
-       </div> );
+       </div>);
     }
-    
-
 }
-
 export default WordCloud;
